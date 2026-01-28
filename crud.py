8c708f0f0573202b9954b5fe9bc8080d09474361
Kozimbek <kozmibrohimov@gmail.com>
2026-01-28 14:46:55 +0500

    Swagger endpointlarni doctors va patients ga ajratildi

--- crud.py
+++ crud.py
@@ -40,3 +40,42 @@ async def delete_doctor(doctor_id:int,db:AsyncSession):
     await db.delete(db_doctor)
     await db.commit()
     return {"message":"Doctor delete seccesful"}
+
+
+########################################################################
+async def create_patient(doctor:PatientCreate,db:AsyncSession) -> PatientResponse:
+    db_patient=Patient(**patient.model_dump())
+    db.add(db_patient)
+    await db.commit()
+    await db.refresh(db_patient)
+    return DoctorResponse.model_validate(db_patient)
+
+async def read_patients(db:AsyncSession) -> list[PatientResponse]:
+    data = await db.execute(select(Patient))
+    patients=data.scalars().all()
+    return [PatientResponse.model_validate(patient) for patient in patients]
+
+async def read_patient(patient_id:int,db:AsyncSession) -> PatientResponse:
+    db_patient=await db.get(Patient,patient_id)
+    if db_patient is None:
+        raise HTTPException(status_code=404,detail="patient not found")
+    return PatientResponse.model_validate(db_patient)
+
+async def update_patient(patient_id:int,patient:PatientCreate,db:AsyncSession) -> PatientResponse:
+    db_patient = await db.get(Patient, patient_id)
+    if db_patient is None:
+        raise HTTPException(status_code=404, detail="patient not found")
+    for key , value in patient:
+        setattr(db_patient,key,value)
+
+    await db.commit()
+    await db.refresh(db_patient)
+    return PatientResponse.model_validate(db_patient)
+
+async def delete_patient(patient_id:int,db:AsyncSession):
+    db_patient = await db.get(Patient, patient_id)
+    if db_patient is None:
+        raise HTTPException(status_code=404, detail="patient not found")
+    await db.delete(db_patient)
+    await db.commit()
+    return {"message":"patient delete seccesful"}
--- main.py
+++ main.py
@@ -17,25 +17,58 @@ async def init_db():
 async def startup_event():
     await init_db()
 
-@app.post("/doctors/",response_model=DoctorResponse)
+@app.post("/doctors/",response_model=DoctorResponse,tags=["Doctors"])
 async def create_doctor_endpoint(doctor:DoctorCreate,db:AsyncSession=Depends(get_db)):
     return await crud.create_doctor(doctor,db)
 
-@app.get("/doctors/",response_model=list[DoctorResponse])
+@app.get("/doctors/",response_model=list[DoctorResponse],tags=["Doctors"])
 async def get_all(db:AsyncSession=Depends(get_db)):
     return await crud.read_doctors(db)
 
-@app.get("/doctors/{doctor_id}/",response_model=DoctorResponse)
+@app.get("/doctors/{doctor_id}/",response_model=DoctorResponse,tags=["Doctors"])
 async def get_one(doctor_id:int,db:AsyncSession=Depends(get_db)):
     return await crud.read_doctor(doctor_id,db)
 
-@app.put("/doctors/{doctor_id}/",response_model=DoctorResponse)
+@app.put("/doctors/{doctor_id}/",response_model=DoctorResponse,tags=["Doctors"])
 async def update(doctor_id:int,doctor:DoctorCreate,db:AsyncSession=Depends(get_db)):
     return await crud.update_doctor(doctor_id,doctor,db)
 
-@app.delete("/doctors/{doctor_id}/",response_model=dict)
+@app.delete("/doctors/{doctor_id}/",response_model=dict,tags=["Doctors"])
 async def delete(doctor_id:int,db:AsyncSession=Depends(get_db)):
     return await crud.delete_doctor(doctor_id,db)
+##################################################################################
+
+@app.post("/patients/",response_model=PatientResponse,tags=["Patient"])
+async def create_patient_endpoint(patient:PatientCreate,db:AsyncSession=Depends(get_db)):
+    return await crud.create_patient(patient,db)
+
+@app.get("/patients/",response_model=list[PatientResponse],tags=["Patient"])
+async def get_all(db:AsyncSession=Depends(get_db)):
+    return await crud.read_patients(db)
+
+@app.get("/patients/{patient_id}/",response_model=PatientResponse,tags=["Patient"])
+async def get_one(patient_id:int,db:AsyncSession=Depends(get_db)):
+    return await crud.read_patient(patient_id,db)
+
+@app.put("/patients/{patient_id}/",response_model=PatientResponse,tags=["Patient"])
+async def update(patient_id:int,patient:PatientCreate,db:AsyncSession=Depends(get_db)):
+    return await crud.update_patient(patient_id,patient,db)
+
+@app.delete("/patients/{patient_id}/",response_model=dict,tags=["Patient"])
+async def delete(patient_id:int,db:AsyncSession=Depends(get_db)):
+    return await crud.delete_patient(patient_id,db)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
